d6ac215e4af0be0c7260cbac6cf39033b4c35007
Matt Mastin <49283941+mmastin@users.noreply.github.com>
2019-08-27 15:37:50 -0400

    initial files

---
+++ README.md
@@ -0,0 +1,1 @@
+# BnBalyze-DS-Heroku
---
+++ app.py
@@ -0,0 +1,41 @@
+import numpy as np
+from flask import Flask, jsonify, request
+import pickle
+
+# model
+my_model = pickle.load(open('tuesday_model_6.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+
+def make_predict():
+    #get data
+    data = request.get_json(force=True)
+
+    # testing on tuesday model 6
+    predict_request = [data['neighborhood'],
+                       data['room_type'],
+                       data['accommodates'],
+                       data['bedrooms'],
+                       data['bathrooms'],
+                       data['number_of_reviews'],
+                       data['wifi'],
+                       data['cable_tv'],
+                       data['washer'],
+                       data['kitchen']]
+
+
+    predict_request = np.array(predict_request).reshape(1,-1)
+
+    #preds
+    y_hat = my_model.predict(predict_request)
+
+    # send back to browser
+    output = {'y_hat': int(y_hat[0])}
+    return jsonify(results=output)
+
+
+
+if __name__ == '__main__':
+    app.run(port = 6000, debug=True)
